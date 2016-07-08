db5e08954dbe15d5ff6377f97b64941ac61787b1
Larry Price <larry.price.dev@gmail.com>
2016-07-08 10:53:02 -0400

    Initial commit

---
+++ build-in-chroot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f "$0"))
+
+BIN_DIR=$DIR/bin
+CLICK_DIR=$BIN_DIR/click
+GO_DIR=$DIR/go-installation/go
+GO_BIN_DIR=$GO_DIR/bin
+
+# Recreate click build directory
+
+rm -rf $CLICK_DIR
+mkdir -p $CLICK_DIR
+
+# Remove old click packages
+
+find $CLICK_DIR/.. -name "*.click" -exec rm {} \;
+
+# Build the project
+
+click chroot -a armhf -f ubuntu-sdk-15.04 -s vivid run CGO_ENABLED=1 GOARCH=arm GOARM=7 PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig GOPATH=$GOPATH GOROOT=$GO_DIR PWD=$PWD CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ $GO_BIN_DIR/go build -o $BIN_DIR/linux_arm/ubuntu-go-qml-template -ldflags '-extld=arm-linux-gnueabihf-gcc' -v -x src/main.go
+
+# Copy files into click directory
+
+cp $BIN_DIR/linux_arm/ubuntu-go-qml-template $CLICK_DIR
+cp -R $DIR/share $CLICK_DIR
+cp $DIR/manifest.json $CLICK_DIR
+cp $DIR/ubuntu-go-qml-template.apparmor $CLICK_DIR
+cp $DIR/ubuntu-go-qml-template.desktop $CLICK_DIR
+cp $DIR/ubuntu-go-qml-template.png $CLICK_DIR
+
+# Build click package
+
+cd $CLICK_DIR/..
+click build $CLICK_DIR
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go build -o bin/ubuntu-go-qml-template -v -x src/main.go
